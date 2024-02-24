32fac75c9a887e0cbce6da57311628f61b45c3f6
Justin Kruskie <kruskiej@baisd.net>
2024-02-24 15:37:59 +0000

    Create assignment

---
+++ AstronautInformation.java
@@ -0,0 +1,12 @@
+public class AstronautInformation 
+{
+    public static void main(String[] args)
+    {
+        // Create the variables below
+
+        // Store information for the variables below
+
+        // Display the message below
+        
+    }
+}
