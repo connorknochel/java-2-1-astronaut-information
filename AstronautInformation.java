0a658eaef7ea97cb38d74b9c6399734b75d61788
connorknochel <knochcon2027@gmail.com>
2025-09-10 08:49:27 -0400

    save buzz information

--- AstronautInformation.java
+++ AstronautInformation.java
@@ -2,11 +2,17 @@ public class AstronautInformation {
     
     public static void main(String[] args) {
         // Create the variables below
-
+        String name;
+        int age;
+        String rank = "Colonel";
         // Store information for the variables below
-
+        name = "Buzz Aldrin";
+        double time;
+        time = 12.0784;
+        age = 90;
         // Display the message below
-        
+        System.out.println(name + " is " + age + " years old. "  );
+   System.out.println( "He is a " + rank + " and has spent " + time + " days in space.");
     }
     
 }
